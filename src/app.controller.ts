fd090c11a03bd4a4278245ca2dbbebecfc6e28ee
ali-master <ali_4286@live.com>
2024-05-07 23:12:03 +0330

    feat: add todos

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Body,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { TodoDto } from './dtos/todo.dto';
 
-@Controller()
+@Controller('/todos')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getTodos(@Query() filters: Partial<TodoDto>): Array<TodoDto> {
+    return this.appService.getTodos(filters);
+  }
+
+  @Put('/toggle-done/:id')
+  toggleDone(@Param('id') id: string): Array<TodoDto> {
+    return this.appService.updateTodo(id, {
+      done: !this.appService.getTodos({ id })[0].done,
+    });
+  }
+  @Post()
+  createTodo(@Body() todo: Omit<TodoDto, 'id'>): TodoDto {
+    return this.appService.createTodo(todo);
+  }
+  @Put(':id')
+  updateTodo(
+    @Param('id') id: string,
+    @Body() todo: Omit<TodoDto, 'id'>,
+  ): Array<TodoDto> {
+    return this.appService.updateTodo(id, todo);
+  }
+  @Delete(':id')
+  deleteTodo(@Param('id') id: string): Array<TodoDto> {
+    return this.appService.deleteTodo(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,66 @@
 import { Injectable } from '@nestjs/common';
+import { TodoDto } from './dtos/todo.dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private todos: Array<TodoDto> = [
+    {
+      id: 'f47ac10b-58cc-4372-a567-0e02b2c3d478',
+      title: 'Todo 1',
+      description: 'Description 1',
+      done: false,
+    },
+    {
+      id: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
+      title: 'Todo 2',
+      description: 'Description 2',
+      done: true,
+    },
+  ];
+
+  getTodos(filters: Partial<TodoDto>): Array<TodoDto> {
+    if (!filters) return this.todos;
+
+    return this.todos.filter((todo) => {
+      return Object.entries(filters).every(([key, value]) => {
+        const todoValue = todo[key];
+        if (typeof todoValue === 'string') {
+          return todoValue.includes(value as string);
+        }
+
+        if (typeof todoValue === 'boolean') {
+          value = value === 'true';
+
+          return todoValue === value;
+        }
+
+        return todoValue === value;
+      });
+    });
+  }
+
+  createTodo(todo: Omit<TodoDto, 'id'>): TodoDto {
+    const createdTodo = {
+      id: Math.random().toString(36).substr(2, 9),
+      ...todo,
+    };
+    this.todos.push(createdTodo);
+
+    return createdTodo;
+  }
+
+  deleteTodo(id: string): Array<TodoDto> {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+    return this.todos;
+  }
+
+  updateTodo(id: string, todo: Omit<Partial<TodoDto>, 'id'>): Array<TodoDto> {
+    this.todos = this.todos.map(($todo) => {
+      if ($todo.id === id) {
+        $todo = Object.assign($todo, todo);
+      }
+      return $todo;
+    });
+    return this.todos;
   }
 }
